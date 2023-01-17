64233056fe6095690c84c1f7271cb2682218de12
Mike Margreve <63907112+margrevm@users.noreply.github.com>
2023-01-17 10:54:50 +0100

    Rearranged script from source repo

--- pop-postinstall.sh
+++ pop-postinstall.sh
@@ -1,49 +1,58 @@
 #!/usr/bin/env bash
 
+function confirmation_prompt() {
+    read -p "$1 (y/n) " yn
+    case $yn in 
+        [yY] ) ;;
+        [nN] ) echo exiting...;
+            exit;;
+        * ) echo invalid response;;
+    esac
+}
+
+echo "--------------------------"
+echo " Ubuntu post-installation"
+echo "--------------------------"
+confirmation_prompt "Do you want to run the script now?"
+
+# ---------------------------------------------------
+# Creating folder structure
+# ---------------------------------------------------
+echo "➜ Creating folder structure"
+# Folder for projects (other than source code)
+mkdir -pv $HOME/projects
+# Folder for bash 'general use' scripts
+mkdir -pv $HOME/scripts
+# Folder for source code and repos
+mkdir -pv $HOME/src
+
+# ---------------------------------------------------
+# APT package installation
+# ---------------------------------------------------
 PACKAGE_LIST=(
-	vim
-	zsh
-	slack-desktop
-	discord
 	vlc
 	vlc-plugin-access-extra
-	mcomix3
 	htop
-	gnome-boxes
 	gnome-tweaks
 	python3
-	youtube-dl
 	neofetch
 	nmap
-	pv
 	wget
 	java-latest-openjdk
-	wine
-	radeontop
-	inxi
 	heif-gdk-pixbuf
-		
+	codium
 )
 
-FLATPAK_LIST=(
-	org.telegram.desktop
-	net.veloren.airshipper
-	org.mozilla.firefox
-)
+# Add respositiories
+echo "➜ Add repositories"
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
+echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list 
 
-# gnome settings
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-
-# add flathub repository
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# add third party software
- 
 # update repositories
-
+echo "➜ Updating repositories"
 sudo apt-get update -yq
 
-# iterate through packages and installs them if not already installed
+# iterate through packages and install them if not already installed
 for package_name in ${PACKAGE_LIST[@]}; do
 	if ! sudo apt list --installed | grep -q "^\<$package_name\>"; then
 		echo "installing $package_name..."
@@ -55,29 +64,8 @@ for package_name in ${PACKAGE_LIST[@]}; do
 	fi
 done
 
-for flatpak_name in ${FLATPAK_LIST[@]}; do
-	if ! flatpak list | grep -q $flatpak_name; then
-		flatpak install "$flatpak_name" -y
-	else
-		echo "$package_name already installed"
-	fi
-done
-
-
-# grab codium
-echo "Grabbing VSCode without telemetry"
-sleep 1
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg 
-echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list 
-sudo apt update -yq && sudo apt install codium -yq
-
 # remove default firefox
 sudo apt purge firefox -y
-
-# setup xanmod for better kernel scheduler experience
-echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
-wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
-sudo apt update -y && sudo apt install linux-xanmod -y
 
 # upgrade packages
 sudo apt clean -y
@@ -87,8 +75,51 @@ sudo dpkg --configure -a
 sudo apt full-upgrade -y
 sudo apt autoremove --purge -y
 
+# ---------------------------------------------------
+# Flatpack packages installation
+# ---------------------------------------------------
+FLATPAK_LIST=(
+	org.mozilla.firefox
+	com.vscodium.codium
+)
+
+# add flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+for flatpak_name in ${FLATPAK_LIST[@]}; do
+	if ! flatpak list | grep -q $flatpak_name; then
+		flatpak install "$flatpak_name" -y
+	else
+		echo "$package_name already installed"
+	fi
+done
+
+# ---------------------------------------------------
+# Custom actions
+# ---------------------------------------------------
+# setup xanmod for better kernel scheduler experience
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+sudo apt update -y && sudo apt install linux-xanmod -y
+
+# Update font cache (required after installing MS fonts)
+sudo fc-cache -f
+
+# gnome settings
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+# All gedit related settings can be listed with: gsettings list-recursively | grep -i gedit
+# Display line numbers in gedit
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+# Tab size is 4 spaces
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+
 # grab mullvad
 cd && wget --content-disposition https://mullvad.net/download/app/deb/latest && sudo dpkg -i Mullvad*.deb
 
 # open github to remind me to set up github
 firefox https://github.com
+
+echo "➜ Rebooting system"
+#confirmation_prompt "Do you want to reboot now?"
+#reboot
